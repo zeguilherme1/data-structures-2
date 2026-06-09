917cba56e46632eb6683970d4e153e92737ddda1
Letícia Adriana <leticiaadrianas@gmail.com>
2026-06-09 15:54:12 -0300

    FIX: index search output

--- search/criteria.c
+++ search/criteria.c
@@ -88,7 +88,8 @@ int find_rrn_by_station_code(FILE *index_file, int has_station_code)
 
     int left = 0;
     int right = total - 1;
-
+    
+    fseek(index_file, 1, SEEK_SET);
     //uses binary search to find the correspondent rrn
     while (left <= right)
     {
@@ -131,12 +132,16 @@ Record *indexed_search(FILE *data_file, FILE *index_file, long data_offset, Sear
     if (rrn == NO_DATA_ERROR)
         return NULL;
 
-    fseek(data_file, data_offset, SEEK_SET);
     Record *result_record = read_rrn_record(data_file, rrn);
 
     if (result_record == NULL)
         return NULL;
 
+    if (result_record->removed == TRUE)
+    {
+        free_record(&result_record);
+        return NULL;
+    }
     //validate record after fetching
     if (result_record->removed == FALSE &&
         matches_record_criteria(result_record, criteria, num_fields) == 0)
@@ -167,9 +172,9 @@ Record **search_record(FILE *data_file, FILE *index_file, long data_offset, Sear
         *count = 1;
 
         return results;
+    }else{
+        return sequential_search(data_file, data_offset, criteria, num_fields, count);
     }
-
-    return sequential_search(data_file, data_offset, criteria, num_fields, count);
 }
 
 int criteria_search()
--- utils/string_utils.c
+++ utils/string_utils.c
@@ -58,21 +58,18 @@ void print_string(char *string, int size)
 }
 
 int matches_string(char *criteria_value, char *field, int field_size)
-{
+{	
+	
 	if (strcmp(criteria_value, "NULO") == 0)
 	{
-		if (field_size == 0)
-			return 0;
-		else
-			return -1;
+		if (field_size == 0) return 0;
+		else return -1;
 	}
-	else
-	{
-		if (strcmp(criteria_value, field) != 0)
-			return -1;
-		else
-			return 0;
-	}
+	if (field == NULL) return -1;
+
+    if (strcmp(criteria_value, field) != 0) return -1;
+	
+	return 0;
 }
 
 void clean_string(char *str)
